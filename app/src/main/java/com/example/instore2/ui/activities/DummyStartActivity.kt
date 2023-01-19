58c2ffa00bd5f88c2bd84c49ee7519ca3b2ec533
Abhi <abhishekvelekar79200@gmail.com>
2023-01-19 21:54:41 +0530

    Main Activity Layout added

--- app/src/main/java/com/example/instore2/ui/activities/DummyStartActivity.kt
+++ app/src/main/java/com/example/instore2/ui/activities/DummyStartActivity.kt
@@ -15,6 +15,7 @@ class DummyStartActivity : AppCompatActivity() {
 
         if (!SharePrefs.getInstance(this).getBoolean(SharePrefs.IS_INSTAGRAM_LOGIN)){
             startActivityForResult(Intent(this , LoginActivity::class.java) , 100)
+            startActivity(Intent(this , MainActivity::class.java))
         }
 
     }
