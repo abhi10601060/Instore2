87cb9d24758246da882abab31b24de149db590b0
Abhi <abhishekvelekar79200@gmail.com>
2023-01-19 21:07:27 +0530

    LoginActivity and Shared Preference added

--- app/src/main/java/com/example/instore2/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/instore2/ui/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.instore2
+package com.example.instore2.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.instore2.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
