3d435d228fdddaa6d6ac0d7d239ac832a223b699
Abhi <abhishekvelekar79200@gmail.com>
2023-01-19 22:23:00 +0530

    Network Classes Added

---
+++ app/src/main/java/com/example/instore2/networks/InstaService.kt
@@ -0,0 +1,21 @@
+package com.example.instore2.networks
+
+import com.example.instore2.models.StoryModel
+import com.example.instore2.models.TrayModel
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Url
+
+interface InstaService {
+
+    @GET
+    suspend fun getStories(@Url url : String, @Header("Cookie") str2 : String , @Header("User-Agent")  str3 : String) : Response<StoryModel>
+
+    @GET
+    suspend fun getStoryMedia(@Url url : String, @Header("Cookie") str2 : String , @Header("User-Agent")  str3 : String) : Response<TrayModel>
+
+    @GET
+    suspend fun getUrlData(@Url url : String, @Header("Cookie") str2 : String , @Header("User-Agent")  str3 : String) : Response<TrayModel>
+
+}
