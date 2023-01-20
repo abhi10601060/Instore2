cd5ec23740eeaf09cb89a07a42ce57f0720cf95e
Abhi <abhishekvelekar79200@gmail.com>
2023-01-20 17:08:14 +0530

    retreving videos from external storage feature added

--- app/src/main/java/com/example/instore2/ui/fragments/VideosFragment.kt
+++ app/src/main/java/com/example/instore2/ui/fragments/VideosFragment.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.instore2.R
+import com.example.instore2.adapters.StoredMediaAdapter
 import com.example.instore2.ui.activities.StorageActivity
 import com.example.instore2.viewmodels.StorageViewModel
 
@@ -20,8 +22,10 @@ class VideosFragment : Fragment(R.layout.fragment_videos_layout) {
         videosRV = view.findViewById(R.id.videos_RV)
         storageViewModel = (activity as StorageActivity).storageViewModel
 
-        storageViewModel.photos.observe(viewLifecycleOwner , Observer {
-
+        storageViewModel.videos.observe(viewLifecycleOwner , Observer {videos ->
+            val storedVideosAdapter = context?.let { StoredMediaAdapter(videos , it) }
+            videosRV.adapter = storedVideosAdapter
+            videosRV.layoutManager = GridLayoutManager(context, 2)
         })
 
     }
