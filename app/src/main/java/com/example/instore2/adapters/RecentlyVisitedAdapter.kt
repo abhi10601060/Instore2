878375d7eac93be0b81dba1b11326b2564294a52
Abhi <abhishekvelekar79200@gmail.com>
2023-01-23 16:25:14 +0530

    feature to open recently visited profiles on instagram added

--- app/src/main/java/com/example/instore2/adapters/RecentlyVisitedAdapter.kt
+++ app/src/main/java/com/example/instore2/adapters/RecentlyVisitedAdapter.kt
@@ -1,9 +1,13 @@
 package com.example.instore2.adapters
 
+import android.content.ActivityNotFoundException
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RelativeLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
@@ -26,12 +30,32 @@ class RecentlyVisitedAdapter(val context: Context) : ListAdapter<UserModel, Rece
         Glide.with(context).asBitmap().load(user.profilepicurl).into(holder.image)
 
         holder.recent_name.text = user.username.toString()
+
+        holder.parent.setOnClickListener(View.OnClickListener {
+            launchInstagram(user)
+        })
+    }
+
+    private fun launchInstagram(user: UserModel?) {
+        val uriForInsta = Uri.parse("http://instagram.com/_u/${user?.username}")
+        val intentForInsta = Intent(Intent.ACTION_VIEW, uriForInsta)
+
+        val uriForBrowser = Uri.parse("http://instagram.com/${user?.username}")
+        val intentForBrowser = Intent(Intent.ACTION_VIEW , uriForBrowser)
+
+        try {
+            context.startActivity(intentForInsta)
+        }
+        catch (e : ActivityNotFoundException){
+            context.startActivity(intentForBrowser)
+        }
     }
 
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
         val image = itemView.findViewById<CircleImageView>(R.id.recent_visit_profile_image)
         val recent_name = itemView.findViewById<TextView>(R.id.recent_visit_profile_name)
+        val parent = itemView.findViewById<RelativeLayout>(R.id.recent_visit_parent)
     }
 
     class DiffutilCallback() : DiffUtil.ItemCallback<UserModel>(){
